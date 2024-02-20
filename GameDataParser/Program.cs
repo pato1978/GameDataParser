8aa94ed75ae6e821b6975565f4617a773d76886d
Patrizio Veglia <pveglia@gmx.de>
2024-02-20 13:21:15 +0100

    added first functionality

--- GameDataParser/Program.cs
+++ GameDataParser/Program.cs
@@ -1,4 +1,14 @@
-﻿
+﻿Console.WriteLine("Please enter name of file you want to read:");
+var input = Console.ReadLine();
+
+if (input == null)
+{
+    Console.WriteLine("File name cannot be null.");
+}
+else if ( input == "")
+{
+    Console.WriteLine("File name cannot be empty.");
+}
 
 
 Console.ReadLine();
